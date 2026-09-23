bfcebb37762cfd57846d370f5a6c6ef12ed043d2
Ruturaj Limbkar <limbkarruturaj@gmail.com>
2026-09-23 10:01:33 +0530

    Add About Us component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,24 @@
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Paradise Nursery is an online plant shop dedicated to bringing
+        beautiful and healthy plants to homes and workplaces.
+      </p>
+
+      <p>
+        We offer a variety of indoor and outdoor plants for plant lovers,
+        helping customers create greener and more refreshing spaces.
+      </p>
+
+      <p>
+        Our goal is to make it simple and convenient for customers to
+        discover and purchase plants online.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
